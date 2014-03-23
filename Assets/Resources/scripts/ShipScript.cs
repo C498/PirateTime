064126f69d5877af1add1b071bbf04d68d97b770
Claudine Gladue <gladuec17@macewan.ca>
2014-03-22 21:42:05 -0600

    Implement random generation of new island objects without overlapping.

--- Assets/Resources/scripts/ShipScript.cs
+++ Assets/Resources/scripts/ShipScript.cs
@@ -48,7 +48,7 @@ public class ShipScript : MonoBehaviour
 	public GameObject target = null;	//destination
 	public float smooth = 3.0F;
 	bool newTrip = true; 			//used to indicate a new trip can be started
-	public bool canSail = false; 	//whether can sail to specified destination
+	public bool canSail = true; 	//whether can sail to specified destination
 	public bool shouldRotate = false; //whether the ship should be rotating to face destination
 	public bool startTrip = false;
 	
@@ -68,23 +68,23 @@ public class ShipScript : MonoBehaviour
 	bool questSuccess = false;
 	string resultText = "";
 	bool secondQuestGUIOn = false;
-
+	
 	bool questGUIOn = false;
 	Quests currentQuest;
 	public List<GameObject> islands;
-
-
+	
+	
 	//moving enemy ship
 	public bool enemyNear = false;
-
+	
 	// Use this for initialization
 	void Start ()
 	{
 		islandScript = Resources.Load ("statImages/ship") as IslandScript; 
 		//island = GameObject.FindGameObjectWithTag ("island"); //we need the island data
-
+		
 	}
-
+	
 	GameObject FindClosestShip() {
 		GameObject[] gos;
 		gos = GameObject.FindGameObjectsWithTag("island");
@@ -101,8 +101,8 @@ public class ShipScript : MonoBehaviour
 		}
 		return closest;
 	}
-
-
+	
+	
 	GameObject FindClosestEnemy() {
 		GameObject[] gos;
 		gos = GameObject.FindGameObjectsWithTag("enemy");
@@ -117,7 +117,7 @@ public class ShipScript : MonoBehaviour
 				distance = curDistance;
 			}
 		}
-
+		
 		return closest;
 	}
 	
@@ -126,33 +126,35 @@ public class ShipScript : MonoBehaviour
 	{
 		xCoord = gameObject.transform.position.x;
 		zCoord = gameObject.transform.position.z;
-
+		
 		//DISTANCE CHECKER IF ANY ISLANDS NEAR? (EARLY MECHANIC)
-		distance = Vector3.Distance (transform.position, FindClosestShip().transform.position);
-		if (distance < 6) closeEnough = true; //we are close to an island
-		if (distance > 7) closeEnough = false;
+		GameObject isl = FindClosestShip();
+		if (isl != null){
+			distance = Vector3.Distance (transform.position, isl.transform.position);
+			if (distance < 6) closeEnough = true; //we are close to an island
+			if (distance > 7) closeEnough = false;
+		} else closeEnough = false;
 
 
-		//distance checker if enemies near
-		//check if enemies
-
+		GameObject closestE = null;
+		//check if enemy exists and is close to us
 		if (GameObject.FindGameObjectWithTag("enemy") != null){ 
-			distance = Vector3.Distance (transform.position, FindClosestEnemy().transform.position);
+			closestE = FindClosestEnemy ();
+			distance = Vector3.Distance (transform.position, closestE.transform.position);
 			if (distance < 6) enemyNear = true; //we are close to an island
-			if (distance > 10) enemyNear = false;
-			
-			if (enemyNear) {
-				Debug.Log ("WE ARE CLOSE TO AN ENEMY");
-				chaseShip(FindClosestEnemy());
-			} else Debug.Log ("WE ARENT CLOSE TO ENEMY");
-		} else Debug.Log ("enemy doesnt exist");
+			if (distance > 6) enemyNear = false;
+		} 
 
-
+		if (enemyNear) {
+			chaseShip(closestE);
+		}
 
 		checkMouse ();
+
+		//Debug.Log ("Cansail is " + canSail);
 		if (canSail == true) {
-			sail (); //go there
-			destDistance = Vector3.Distance (transform.position, flag.transform.position); //distance to dest
+			sail ();
+			destDistance = Vector3.Distance (transform.position, flag.transform.position);
 			if (destDistance == 1) destDistance = 0;
 		}
 	}
@@ -161,10 +163,10 @@ public class ShipScript : MonoBehaviour
 	// GUI
 	void OnGUI ()
 	{
-
+		
 		// Box for resources at bottom left
 		GUI.Box(new Rect(10, 610, 400, 52), "");
-
+		
 		Texture2D statIntegrityTexture = Resources.Load ("statImages/" + "integrity") as Texture2D;
 		Texture2D statGunPowderTexture = Resources.Load ("statImages/" + "cannonball") as Texture2D;
 		Texture2D statCrewTexture = Resources.Load ("statImages/" + "crew") as Texture2D;
@@ -176,42 +178,42 @@ public class ShipScript : MonoBehaviour
 		Texture2D statCharismaTexture = Resources.Load ("statImages/" + "charisma") as Texture2D;
 		Texture2D statEvilTexture = Resources.Load ("statImages/" + "evil") as Texture2D;
 		Texture2D statMoraleTexture = Resources.Load ("statImages/" + "morale") as Texture2D;
-
+		
 		GUI.DrawTexture(new Rect(10, 612, 50, 50), statIntegrityTexture, ScaleMode.StretchToFill, true, 10.0F);
 		GUI.Label (new Rect(65, 625, 50, 50), ""+statIntegrity);
-
+		
 		GUI.DrawTexture(new Rect(95, 612, 50, 50), statCrewTexture, ScaleMode.StretchToFill, true, 10.0F);
 		GUI.Label (new Rect(150, 625, 50, 50), ""+statCrewMembers);
-
+		
 		GUI.DrawTexture(new Rect(170, 612, 50, 50), statAleTexture, ScaleMode.StretchToFill, true, 10.0F);
 		GUI.Label (new Rect(225, 625, 50, 50), ""+statAle);
-
+		
 		GUI.DrawTexture(new Rect(245, 612, 50, 50), statGunPowderTexture, ScaleMode.StretchToFill, true, 10.0F);
 		GUI.Label (new Rect(300, 625, 50, 50), ""+statGunPowder);
-
+		
 		GUI.DrawTexture(new Rect(320, 612, 50, 50), statTreasureTexture, ScaleMode.StretchToFill, true, 10.0F);
 		GUI.Label (new Rect(385, 625, 50, 50), ""+statTreasure);
-
-
-
+		
+		
+		
 		//Box for stats at bottom right
 		GUI.Box(new Rect(870, 610, 400, 52), "");
 		int rightBarXLocation = 870;
 		GUI.DrawTexture(new Rect(rightBarXLocation += 20, 612, 50, 50), statMoraleTexture, ScaleMode.StretchToFill, true, 10.0F);
 		GUI.Label (new Rect(rightBarXLocation += 50, 625, 50, 50), ""+statMorale);
-
+		
 		GUI.DrawTexture(new Rect(rightBarXLocation += 20, 612, 50, 50), statEvilTexture, ScaleMode.StretchToFill, true, 10.0F);
 		GUI.Label (new Rect(rightBarXLocation += 50, 625, 50, 50), ""+statEvil);
-
+		
 		GUI.DrawTexture(new Rect(rightBarXLocation += 20, 612, 50, 50), statCharismaTexture, ScaleMode.StretchToFill, true, 10.0F);
 		GUI.Label (new Rect(rightBarXLocation += 50, 625, 50, 50), ""+statCharisma);
-
+		
 		GUI.DrawTexture(new Rect(rightBarXLocation += 20, 612, 50, 50), statNotorietyTexture, ScaleMode.StretchToFill, true, 10.0F);
 		GUI.Label (new Rect(rightBarXLocation += 50, 625, 50, 50), ""+statNotoriety);
-
+		
 		GUI.DrawTexture(new Rect(rightBarXLocation += 20, 612, 50, 50), statWitTexture, ScaleMode.StretchToFill, true, 10.0F);
 		GUI.Label (new Rect(rightBarXLocation += 50, 625, 50, 50), ""+statWit);
-
+		
 		var oldColor = GUI.backgroundColor;
 		// turn GUI white if we can 'investigate'
 		if (closeEnough == true) {
@@ -249,27 +251,27 @@ public class ShipScript : MonoBehaviour
 		//GUI.Button (new Rect (1000, 10, 200, 400), rightSideMessage);
 		
 		GUI.backgroundColor = oldColor;
-
+		
 		
 		if (questGUIOn){
 			GUIStyle boxStyle = "box";
 			boxStyle.wordWrap = true;
 			GUI.Box (new Rect (400, 100, 420, 430), ""); //outer event box
-
+			
 			//quest image
 			string imagename = currentQuest.eventName.ToString().TrimEnd( '\r', '\n' );
 			Texture2D questTexture = Resources.Load ("questImages/" + imagename) as Texture2D;
 			GUI.DrawTexture(new Rect (460, 110, 300, 220), questTexture, ScaleMode.StretchToFill, true, 10.0F);
-
+			
 			//quest text
 			GUI.Label (new Rect (415, 340, 410, 50 ), currentQuest.eventText);
-
+			
 			GUI.Label (new Rect (575, 390, 200, 50 ), "Options:");
-
+			
 			//first option
 			if (GUI.Button (new Rect (410, 420, 400, 40), currentQuest.optionText1 )) {
 				resultHandler(1);
-
+				
 				if (questSuccess == true) {
 					resultText = currentQuest.successText1;
 					//results off success
@@ -283,11 +285,11 @@ public class ShipScript : MonoBehaviour
 					statNotoriety += currentQuest.successNotoriety1;
 					statTreasure += currentQuest.successCoins1;
 					statWit += currentQuest.successWit1;
-				
-
+					
+					
 				}else {
 					resultText = currentQuest.failText1;
-
+					
 					//results off fail
 					//results off failure
 					statAle += currentQuest.failureAle1;
@@ -300,10 +302,10 @@ public class ShipScript : MonoBehaviour
 					statNotoriety += currentQuest.failureNotoriety1;
 					statTreasure += currentQuest.failureCoins1;
 					statWit += currentQuest.failureWit1;
-
-
+					
+					
 				}
-
+				
 				secondQuestGUIOn = true;
 				questGUIOn = false;
 			}
@@ -311,12 +313,12 @@ public class ShipScript : MonoBehaviour
 			Debug.Log(currentQuest.optionText2);
 			Debug.Log(resultText);
 			if (GUI.Button (new Rect (410, 480, 400, 40), currentQuest.optionText2)) {
-
+				
 				resultHandler(2);
-
+				
 				if (questSuccess == true) {
 					resultText = currentQuest.successText2;
-
+					
 					//results off success
 					statAle += currentQuest.successAle2;
 					statCharisma += currentQuest.successCharisma2;
@@ -328,10 +330,10 @@ public class ShipScript : MonoBehaviour
 					statNotoriety += currentQuest.successNotoriety2;
 					statTreasure += currentQuest.successCoins2;
 					statWit += currentQuest.successWit2;
-
+					
 				} else {
 					resultText = currentQuest.failText2;
-
+					
 					//results off failure
 					statAle += currentQuest.failureAle2;
 					statCharisma += currentQuest.failureCharisma2;
@@ -343,18 +345,18 @@ public class ShipScript : MonoBehaviour
 					statNotoriety += currentQuest.failureNotoriety2;
 					statTreasure += currentQuest.failureCoins2;
 					statWit += currentQuest.failureWit2;
-
-
+					
+					
 				}
-
+				
 				secondQuestGUIOn = true;
 				questGUIOn = false;
 			}
-
+			
 		}
-
-
-
+		
+		
+		
 		if (secondQuestGUIOn){
 			GUIStyle boxStyle = "box";
 			boxStyle.wordWrap = true;
@@ -394,7 +396,7 @@ public class ShipScript : MonoBehaviour
 	public void investigate ()
 	{
 		
-/*		rightSideMessage = "island coords: " + (int)islandScript.xCoord + " " + (int)islandScript.zCoord + "\n" +
+		/*		rightSideMessage = "island coords: " + (int)islandScript.xCoord + " " + (int)islandScript.zCoord + "\n" +
 			"my coords: " + (int)xCoord + " " + (int)zCoord;*/
 		
 		CameraScript cScript;
@@ -404,8 +406,8 @@ public class ShipScript : MonoBehaviour
 		currentQuest = cScript.questList[randomQuestNumber];
 		
 		questGUIOn = true;
-
-
+		
+		
 		//quest stuff
 		
 	}
@@ -452,14 +454,15 @@ public class ShipScript : MonoBehaviour
 		
 		
 		if (Input.GetMouseButtonDown (0) ) {
+			Debug.Log("WE CLICKED THE MOUSE");
 			Vector2 mouse = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
 			
 			//if clicked outside of event gui, turn it off
-			if (new Rect (400, 100, 500, 500).Contains (mouse) && questGUIOn){
+			if (new Rect (400, 100, 500, 500).Contains (mouse) && secondQuestGUIOn){
 				canSail = false;
-				} else { 
-					questGUIOn = false;  
-					secondQuestGUIOn = false;}
+			} else {  
+				secondQuestGUIOn = false;
+			}
 			
 			//if clicked outside of gui, sail there.
 			if (new Rect (10, 10, 200, 400).Contains (mouse)){
@@ -495,23 +498,26 @@ public class ShipScript : MonoBehaviour
 	
 	public void resultHandler (int choice){
 		int diceRoll = Random.Range (1, 100); //for ints
-
-				if (choice == 1){
-					if (diceRoll >= currentQuest.odds1) {
-						questSuccess = true;
-					} else questSuccess = false;
-
-
-				}else if (choice == 2){
-					if (diceRoll >= currentQuest.odds2) {
-						questSuccess = true;
-					} else questSuccess = false;
-
-				} else { Debug.Log ("error"); }
-
-
-			}
+		
+		if (choice == 1){
+			if (diceRoll >= currentQuest.odds1) {
+				questSuccess = true;
+			} else questSuccess = false;
+			
+			
+		}else if (choice == 2){
+			if (diceRoll >= currentQuest.odds2) {
+				questSuccess = true;
+			} else questSuccess = false;
+			
+		} else { Debug.Log ("error"); }
+		
+		
+	}
+	
+	
 	public void chaseShip(GameObject enemy){
+		Debug.Log ("ship should chase us");
 		//destination is 
 		//rotate enemy
 		Quaternion rotation = Quaternion.LookRotation (transform.position - enemy.transform.position);
@@ -526,5 +532,5 @@ public class ShipScript : MonoBehaviour
 		}
 	}
 	
-
+	
 }
